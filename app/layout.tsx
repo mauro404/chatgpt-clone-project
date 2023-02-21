d7ce367cbda3837f9c756bf21bb9225c8785d47b
mauro404 <mau.mansano@gmail.com>
2023-02-21 18:30:09 +0100

    create homepage with heroicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import SideBar from '../components/SideBar'
 import '../styles/globals.css'
 
 export default function RootLayout({
@@ -8,7 +9,14 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <div className='flex'>
+          <SideBar />
+          {/* Client Provider - Notification */}
+
+          <div className='bg-[#343541] flex-1'>{children}</div>
+        </div>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,53 @@
+import {
+    SunIcon,
+    BoltIcon,
+    ExclamationTriangleIcon,
+  } from '@heroicons/react/24/outline';
+
 function HomePage() {
   return (
-    <div>
-        <h1>ChatGPT Messenger</h1>
+    <div className="flex flex-col items-center justify-center text-white h-screen px-2">
+        <h1 className="text-5xl font-bold mb-20">ChatGPT </h1>
+        <div className="flex space-x-2 text-center">
+            <div>
+                <div className="flex flex-col items-center justify-center mb-5">
+                    <SunIcon className="h-8 w-8" />
+                    <h2>Examples</h2>
+                </div>
 
+                <div className="space-y-2">
+                    <p className="infoText">"What is the difference between npm and yarn?"</p>
+                    <p className="infoText">"What are the best coding practices to follow?"</p>
+                    <p className="infoText">"Should I use Flexbox or Grid on my project"</p>
+                </div>
+            </div>
+
+            <div>
+                <div className="flex flex-col items-center justify-center mb-5">
+                    <BoltIcon className="h-8 w-8" />
+                    <h2>Capabilities</h2>
+                </div>
+
+                <div className="space-y-2">
+                    <p className="infoText">This ChatGPT Clone was built using TypeScript and Next.js 13</p>
+                    <p className="infoText">Tailwind CSS for styling and Firebase on the backend</p>
+                    <p className="infoText">Hot Toast Notification while ChatGPT is thinking</p>
+                </div>
+            </div>
+
+            <div>
+                <div className="flex flex-col items-center justify-center mb-5">
+                    <ExclamationTriangleIcon className="h-8 w-8" />
+                    <h2>Limitations</h2>
+                </div>
+
+                <div className="space-y-2">
+                    <p className="infoText">May occasionally generate incorrect information</p>
+                    <p className="infoText">May occasionally produce harmful instructions or biased content</p>
+                    <p className="infoText">Limited knowledge of world and events after 2021</p>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
