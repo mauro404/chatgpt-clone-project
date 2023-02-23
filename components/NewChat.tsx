f4c399332880d88cfe659587263929319e3b6250
mauro404 <mau.mansano@gmail.com>
2023-02-23 14:33:25 +0100

    create ChatRow component +update SideBar and NewChat

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,13 +1,34 @@
-import { PlusIcon } from "@heroicons/react/24/solid"
+"useClient";
+
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { db } from "../firebase";
 
 function NewChat() {
-  return (
-    <div className="border border-gray-700 chatRow">
-        <PlusIcon className="h-4 w-4" />
+  const router = useRouter();
+  const { data: session } = useSession();
 
-        <p>New Chat</p>
+  const createNewChat = async () => {
+    const doc = await addDoc(
+      collection(db, "users", session?.user?.email!, "chats"),
+      {
+        userId: session?.user?.email!,
+        createdAt: serverTimestamp(),
+      }
+    );
+
+    router.push(`/chat/${doc.id}`);
+  };
+
+  return (
+    <div onClick={createNewChat} className="border border-gray-700 chatRow">
+      <PlusIcon className="h-4 w-4" />
+
+      <p>New Chat</p>
     </div>
-  )
+  );
 }
 
-export default NewChat
+export default NewChat;
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,22 +1,45 @@
-import NewChat from "./NewChat"
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import { collection, query, orderBy } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import NewChat from "./NewChat";
+import ChatRow from "./ChatRow";
 
 function SideBar() {
+  const { data: session } = useSession();
+
+  const [chats, loading] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session.user?.email!, "chats"),
+        orderBy("createdAt", "desc")
+      )
+  );
+
   return (
     <div className="p-2 flex flex-col h-screen">
-        <div className="flex-1">
-            <div>
-                <NewChat/>
+      <div className="flex-1">
+        <div>
+          <NewChat />
 
-                <div>
-                    {/* model selection */}
-                </div>
+          <div>{/* model selection */}</div>
 
-                {/* Map through the ChatRows */}
-
-            </div>
+          {chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
         </div>
+      </div>
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt={session.user?.name!}
+          className="h-12 w-12 hover:opacity-50 rounded-full cursor-pointer mx-auto mb-2"
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
