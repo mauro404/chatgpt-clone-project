e549e7238a1904b9418161895b42102a48a93c13
mauro404 <mau.mansano@gmail.com>
2023-03-29 11:29:15 +0200

    update login view

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,22 +1,25 @@
-'use client';
-import { signIn } from 'next-auth/react';
-import Image from 'next/image';
+"use client";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 function Login() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center  text-center bg-[#11A37F]">
-        <button
-            onClick={() => signIn('google')}
-            className="text-white font-bold text-3xl animate-pulse"
-        >
-            <Image
-                src="/chatgpt_logo.png"
-                width={500}
-                height={500}
-                alt="logo"
-            />
-            Sing In To Use ChatGpt
-        </button>
+    <div className="h-screen flex flex-col items-center justify-center  text-center bg-[#11A37F] ">
+      <Image
+        src="/chatgpt_logo.png"
+        width={500}
+        height={500}
+        alt="logo"
+        className="animate-pulse"
+      />
+      <button
+        onClick={() => signIn("google")}
+        className="text-white font-semibold md:font-bold text-xl md:text-3xl "
+      >
+        <p className="border border-white rounded-lg p-4 md:p-8">
+          Click Here to Sing In
+        </p>
+      </button>
     </div>
   );
 }
